3c98e51eb4c5b0e47f3a087a4dd2086885534236
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2016-11-09 11:54:54 -0200

    adding more actions to users resource

--- lib/ejabberd/rest/resources/users_resource.rb
+++ lib/ejabberd/rest/resources/users_resource.rb
@@ -9,9 +9,24 @@ module Ejabberd
             handler(200) { |response| true }
           end
 
-          action :registered_users, 'POST /api/registered_users' do
+          action :registered, 'POST /api/registered_users' do
             body { |object| JSON.generate(object) }
             handler(200) { |response| JSON.parse(response.body) }
+          end
+
+          action :kick_user, 'POST /api/kick_user' do
+            body { |object| JSON.generate(object) }
+            handler(200) { |response| JSON.parse(response.body).with_indifferent_access(:num_resources).to_int }
+          end
+
+          action :rooms, 'POST /api/get_user_rooms' do
+            body { |object| JSON.generate(object) }
+            handler(200) { |response| JSON.parse(response.body) }
+          end
+
+          action :ban_account, 'POST /api/ban_account' do
+            body { |object| JSON.generate(object) }
+            handler(200) { |response| true }
           end
         end
 
--- spec/ejabberd/rest/resources/users_resource_spec.rb
+++ spec/ejabberd/rest/resources/users_resource_spec.rb
@@ -8,8 +8,26 @@ RSpec.describe Ejabberd::Rest::Resources::UsersResource, resource_kit: true do
   end
 
   it 'get registered users of a host' do
-    expect(resource).to have_action(:registered_users).that_handles(200).at_path('/api/registered_users').with_verb(:post) do |handled|
+    expect(resource).to have_action(:registered).that_handles(200).at_path('/api/registered_users').with_verb(:post) do |handled|
       expect(handled).to be_kind_of(Array)
+    end
+  end
+
+  it 'disconnect user’s active sessions' do
+    expect(resource).to have_action(:kick_user).that_handles(200).at_path('/api/kick_user').with_verb(:post) do |handled|
+      expect(handled).to be_kind_of(Integer)
+    end
+  end
+
+  it 'get the list of rooms where this user is occupant' do
+    expect(resource).to have_action(:rooms).that_handles(200).at_path('/api/get_user_rooms').with_verb(:post) do |handled|
+      expect(handled).to be_kind_of(Array)
+    end
+  end
+
+  it 'ban an account: kick sessions and set random password' do
+    expect(resource).to have_action(:ban_account).that_handles(200).at_path('/api/ban_account').with_verb(:post) do |handled|
+      expect(handled).to be_kind_of(BooleanMatcher)
     end
   end
 
