f6fd8ed2b6c5f7e6ee31d0859ca88f2dc00feae6
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2016-11-09 12:50:16 -0200

    delete test.rb

--- test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'lib/ejabberd'
-
-ejabberd_client = Ejabberd::Rest::Client.new({
-                                                 jid: 'admin@im.juster.com.br',
-                                                 password: 'ToEm69ungMjcAe',
-                                                 ejabberd_api: 'http://im.jusster.com.br:5280'
-                                             })
-
-p ejabberd_client.is_running?
