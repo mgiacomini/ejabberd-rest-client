1b150658884ce5cd415a5e2022ab5f4c342d4b7f
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2016-11-09 09:34:37 -0200

    adding status resource and unit tests

--- spec/ejabberd/rest/client_spec.rb
+++ spec/ejabberd/rest/client_spec.rb
@@ -4,8 +4,4 @@ describe ::Ejabberd::Rest::Client do
   it 'has a version number' do
     expect(Ejabberd::Rest::Client::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
